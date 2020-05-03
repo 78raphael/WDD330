f328b03a01f1571ddf78f97dc603cd565a1a9099
J.Johnson <49891794+78raphael@users.noreply.github.com>
2020-05-02 22:41:42 -0700

    Week 2 exercises

--- w2/js/w2.js
+++ w2/js/w2.js
@@ -24,3 +24,63 @@ const exerciseTwo = () => {
     }
   }
 }
+
+const exerciseThree = () => {
+  console.clear();
+  let size = 8;
+  let board = "";
+  let a = "#", b = " ";
+  for(let i = 1; i <= size; i++) {
+    for(let m = 1; m <= size; m++) {
+      board += (m % 2 == 0) ? a : b;
+      if(m == size) {
+        board += "\n";
+        let c = a;
+        a = b;
+        b = c;
+      }
+    }
+  }
+  console.log(board);
+}
+
+const exerciseFour = ($first, $second) => {
+  console.clear();
+  let minX = Math.min($first, $second);
+  console.log(minX);
+
+}
+
+const isEven = (x) => {
+  console.clear();
+
+  if(x === 0)  {
+    console.log("true");
+  } else if(x === 1) {
+    console.log("false");
+  } else {
+    isEven(x - 2);
+  }
+}
+
+const exerciseSix = () => {
+  console.clear();
+  let arr = [];
+  let total = 0;
+
+  const range = (start, end) => {
+    for(let i = start; i <= end; i++) {
+      arr.push(i);
+    }
+  }
+
+  const sum = (arr) => {
+    arr.forEach(value => {
+      total += value;
+    }) 
+    return total;
+  }
+
+  range(1, 10);
+  console.log(sum(arr));
+}
