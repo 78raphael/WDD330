f342b8e8a31e0f8f65de4a8e7b6487a650cfdcc8
J.Johnson <49891794+78raphael@users.noreply.github.com>
2020-05-30 07:06:24 -0700

    Week 6; last nights changes

--- w6/ToDos.js
+++ w6/ToDos.js
@@ -1,29 +1,37 @@
 import { toDoList } from "./toDoList.js";
 import * as utl from "./utilities.js";
 
-const btn = document.getElementById('task-btn');
+const add = document.getElementById('btn-add');
+const tsk = document.getElementById('btn-tsk');
+const all = document.getElementById('btn-all');
+const atv = document.getElementById('btn-atv');
+const cpt = document.getElementById('btn-cpt');
+const num = document.getElementById('tsk-num');
 const taskStorage = window.localStorage;
 let tblList = [];
-
-console.log("taskStorage Length: ", taskStorage.length);
 
 if(taskStorage.length == 0) {
   taskStorage.clear();
 } else {
   tblList = JSON.parse(taskStorage.getItem("tableList"));
+  let list_length = tblList.length;
+  console.log("# of tasks: ", list_length);
+
+  num.innerHTML = list_length;
 
   tblList.forEach(item => {
     utl.addTaskToTable(item);
   });
 }
 
-btn.addEventListener("click", function()  {
+add.addEventListener("click", function()  {
   let entry = document.getElementById('task-input').value;
 
   if(!entry)  {
     alert("Enter a valid task name")
   } else {
     document.querySelector("tbody").innerHTML = "";
+
     let newTask = new toDoList(entry, false);
     tblList.push(newTask);
 
@@ -32,5 +40,27 @@ btn.addEventListener("click", function()  {
     });
 
     taskStorage.setItem('tableList', JSON.stringify(tblList));
+  }
+});
+
+tsk.addEventListener("click", function()  {
+  console.log("Task clicked");
+});
+
+all.addEventListener("click", function()  {
+  console.log("All clicked");
+});
+
+atv.addEventListener("click", function()  {
+  console.log("Active clicked");
+});
+
+cpt.addEventListener("click", function()  {
+  console.log("Completed clicked");
+});
+
+document.addEventListener("click", function(e) {
+  if(e.path[0].id != null)  {
+    console.log("clicked", e.path[0].id);
   }
 });
--- w6/utilities.js
+++ w6/utilities.js
@@ -1,6 +1,17 @@
 
 export function addTaskToTable(taskObj) {
-  let taskToTable = '<tr><td><input type="checkbox" class="" id="'+taskObj.id+'"></td><td>'+taskObj.content+'</td><td>X</td></tr>';
+  let taskToTable = `
+    <tr>
+      <td>
+        <input type="checkbox" class="" id="` + taskObj.id + `">
+      </td>
+      <td>
+        ` + taskObj.content + `
+      </td>
+      <td>
+        <div class="btn-del" id="` + taskObj.id + `">X</div>
+      </td>
+    </tr>`;
 
   document.querySelector("tbody").innerHTML += taskToTable;
 }
